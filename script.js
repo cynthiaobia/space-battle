86054c871835ac833ba26be3b1e22ee1f6c6a6fe
Cynthia <cynthia.obia@gmail.com>
2023-07-15 16:20:58 -0500

    make alien ship factory that generates new ships

--- script.js
+++ script.js
@@ -12,6 +12,31 @@ class Ship {
     }
 }
 
-const ussAssembly = new Ship(20, 5, .7);
+// const ussAssembly = new Ship(20, 5, .7);
 
-console.log(ussAssembly);
+// console.log(ussAssembly);
+
+// make class for our ship
+
+class alienShips {
+    constructor(){
+        this.ships = []
+    }
+
+    generateAlienShip (){
+        const newShip = new Ship();
+        this.ships.push(newShip);
+    }
+}
+
+const aliens = new alienShips();
+
+//generate alien ships and add to array of ships
+for (let i = 0; i <= 5; i++) {
+    aliens.generateAlienShip();
+}
+// print array of ships
+console.log(aliens);
+// print 1 ship from array
+console.log(aliens.ships[1]);
+console.log(aliens.ships[2]);
