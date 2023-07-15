b8c604f8f660218faa7b53aafecd4b8d91e998cd
Cynthia <cynthia.obia@gmail.com>
2023-07-15 15:43:12 -0500

    created ship constructor, tested on USS Assembly

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 // start with actors then actions
 // actors: our spaceship, alien spaceships
 // actions: attack, retreat
+
+// make ship class
+
+class Ship {
+    constructor(hull, firepower, accuracy){
+        this.hull = hull;
+        this.firepower = firepower;
+        this.accuracy = accuracy;
+    }
+}
+
+const ussAssembly = new Ship(20, 5, .7);
+
+console.log(ussAssembly);
